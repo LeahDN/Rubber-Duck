2981c2b81ec1931fa04de724ad25ddf8eaf0dc66
Leah N <leah.niebuhr@gmail.com>
2023-06-13 10:09:27 -0500

    Initial Commit

---
+++ Rubber Duck/Rubber_DuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rubber_DuckApp.swift
+//  Rubber Duck
+//
+//  Created by Scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Rubber_DuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
